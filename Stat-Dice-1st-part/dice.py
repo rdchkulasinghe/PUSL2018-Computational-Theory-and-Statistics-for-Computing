a4607577fea4bbd90e26ebe344cb7972822e53a8
Maheshika Sandamini <131543563+IPMSand@users.noreply.github.com>
2025-01-04 16:20:20 +0530

    changed code details

--- Stat-Dice-1st-part/dice.py
+++ Stat-Dice-1st-part/dice.py
@@ -1,14 +1,12 @@
 # dice 1st part
 def count_combinations(n_dice, target_sum):
     """
-       This function does something.
-
+       This function count all the possible combinations of getting sum of 30
        Args:
-           n_dice: The number of dice using.
-           target_sum: The target value of sum.
-
+            :param n_dice: The number of dice using.
+            :param target_sum: The target value of sum.
        Returns:
-           The count of the all favourable outcomes
+            The count of the all favourable outcomes
        """
     if n_dice == 0:
         return 1 if target_sum == 0 else 0
@@ -28,18 +26,15 @@ sum_of_all_probabilities = probability + un_fav_probability  # probabilities sum
 
 
 print("Answer: ")
-
 # 10 dice throws add up to sum of 30
 print("All outcomes 6^6: ", 6**10)
 print(f"The all favourable outcomes: {favorable_outcomes}")
 print(f"The Probability of getting sum 30 : {probability}")
 
-print()  # a blank line
-
 # extra details
+'''
 print(f"All unfavourable Outcomes: {unfavorable_outcomes}")
 print(f"The probability of not getting 30 sum: {un_fav_probability}")
-
 print()  # a blank line
-
 print(f"Sum of probabilities: {sum_of_all_probabilities}")  # 1
+'''
